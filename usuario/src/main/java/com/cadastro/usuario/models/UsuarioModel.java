6f06ab14b0d6564c59239831bc8b709e1e5a6b25
Neto Vieira <128878577+netovieira1@users.noreply.github.com>
2025-03-13 03:59:12 -0300

    Cria entidade usuarioModels

--- usuario/src/main/java/com/cadastro/usuario/models/UsuarioModel.java
+++ usuario/src/main/java/com/cadastro/usuario/models/UsuarioModel.java
@@ -1,8 +1,14 @@
 package com.cadastro.usuario.models;
 
+import jakarta.persistence.*;
 
+//Entity transforma uma classe em uma entidade do DB
+@Entity
+@Table(name = "tb_cadastro")
 public class UsuarioModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     String nome;
