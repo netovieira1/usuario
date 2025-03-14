8a650aee667509e6964a7d57056a64d5e6455b0d
Neto Vieira <128878577+netovieira1@users.noreply.github.com>
2025-03-14 03:34:57 -0300

    Modelagem banco de dados Usuario e Produtos, anotations feitas para tabelas

---
+++ usuario/src/main/java/com/cadastro/usuario/models/usuarios/Usuario.java
@@ -0,0 +1,69 @@
+package com.cadastro.usuario.models.usuarios;
+
+import com.cadastro.usuario.models.produtos.Produtos;
+import jakarta.persistence.*;
+
+
+//Entity transforma uma classe em uma entidade do DB
+@Entity
+@Table(name = "tb_cadastro")
+public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    //Um usuario tem vários produtos
+    @ManyToMany
+    @JoinTable(name = "usuario_produtos",
+    joinColumns = @JoinColumn(name = "usuario_id"),
+    inverseJoinColumns = @JoinColumn(name = ""))
+    private Produtos produtos;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
